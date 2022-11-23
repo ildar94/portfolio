799d339b9d93512164fdf69a193600d82c382e77
Rustam Gilyazev <rustam_lord@inbox.ru>
2022-11-23 16:58:38 +0300

    add docker-compose_prod for frontend

--- frontend/src/pages/index/index.vue
+++ frontend/src/pages/index/index.vue
@@ -3,7 +3,7 @@
 	<div class="container">
 		<div class="keyVal keyVal_row">
 			<div class="keyVal__item">
-				<h3 class="keyVal__key">Гарантия 1 год</h3>
+				<h3 class="keyVal__key">Гарантия 2 годa</h3>
 				<span class="keyVal__val">на весь ассортимент</span>
 			</div>
 			<div class="keyVal__item">
