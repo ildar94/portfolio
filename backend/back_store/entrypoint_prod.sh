3de40569bb413fd6c0f307277c945d45db1aacee
Ildar Gilyazev <ildar@immortallester1.fvds.ru>
2022-11-28 15:14:40 +0300

    commit from server

--- backend/back_store/entrypoint_prod.sh
+++ backend/back_store/entrypoint_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 gunicorn back_store.wsgi:application --bind 0.0.0.0:8000
