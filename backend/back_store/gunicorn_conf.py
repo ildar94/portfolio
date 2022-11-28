b4ca6e0f05c4dd34c1a8f24d25e80b7d28b49098
Rustam Gilyazev <rustam_lord@inbox.ru>
2022-11-28 19:28:54 +0300

    change upstream name

--- backend/back_store/gunicorn_conf.py
+++ backend/back_store/gunicorn_conf.py
@@ -1,7 +1,7 @@
 bind = '127.0.0.1:8000'
 backlog = 2048
-
-errorlog = '/usr/src/app/'
+worker_class = 'sync'
 loglevel = 'debug'
-accesslog = '-'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = '/var/log/gunicorn/access_log_back_store'
+acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+errorlog =  '/var/log/gunicorn/error_log_back_store'
