31f320e8cbff4de2d82a1143bfb3c5f661ba6e78
Rustam Gilyazev <rustam_lord@inbox.ru>
2022-11-28 19:23:26 +0300

    gunicorn log

--- backend/back_store/gunicorn_conf.py
+++ backend/back_store/gunicorn_conf.py
@@ -1,7 +1,7 @@
 bind = '127.0.0.1:8000'
 backlog = 2048
 
-errorlog = '-'
+errorlog = '/var/log/gunicorn/'
 loglevel = 'debug'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
