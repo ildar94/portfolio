3d394634e5661a9040dcb0a751738d6b2dc8baef
Rustam Gilyazev <rustam_lord@inbox.ru>
2022-11-28 15:28:10 +0300

    static root add

--- backend/back_store/back_store/settings.py
+++ backend/back_store/back_store/settings.py
@@ -143,15 +143,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -178,4 +178,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://gilyazevs.ru:8000",
     "http://127.0.0.1",
     "http://127.0.0.1:8000"
+    "http://localhost:80",
+
 ]
