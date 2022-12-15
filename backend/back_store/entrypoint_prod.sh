80c69b06f9cec6661596f305b76935a4a0230426
Ildar Gilyazev <ildar@immortallester1.fvds.ru>
2022-12-15 17:44:18 +0300

    from server

--- backend/back_store/entrypoint_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn -c gunicorn_conf.py back_store.wsgi:application
